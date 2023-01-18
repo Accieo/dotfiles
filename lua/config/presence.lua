bc74484664ca21c6b2eac932746140450b564ab5
Accieo <42718776+Accieo@users.noreply.github.com>
2023-01-18 21:18:24 +0100

    Updated presence string

--- lua/config/presence.lua
+++ lua/config/presence.lua
@@ -1,6 +1,9 @@
 require('presence'):setup({
 	buttons = false,
-	editing_text = 'Editing a file',
+	editing_text = function(filename)
+		local extension = string.match(filename, "%.([^%.]+)$")
+		return extension and 'Editing a ' .. extension .. ' file' or 'Editing a file'
+	end,
 	file_explorer_text = 'Browsing files',
 	git_commit_text = 'Commiting changes',
 	plugin_manager_text = 'Managing plugins',
