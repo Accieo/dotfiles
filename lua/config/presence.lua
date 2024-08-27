1ea2c124f29ade98a3f37d464572ca201894a296
Accieo <42718776+Accieo@users.noreply.github.com>
2024-08-27 19:04:05 +0200

    Update presence settings

--- lua/config/presence.lua
+++ lua/config/presence.lua
@@ -2,13 +2,13 @@ require('presence'):setup({
 	buttons = false,
 	editing_text = function(filename)
 		local extension = string.match(filename, "%.([^%.]+)$")
-		return 'Editing ' .. filename
-		-- return extension and 'Editing a .' .. extension .. ' file' or 'Editing a file'
+		-- return 'Editing ' .. filename
+		return extension and 'Editing a .' .. extension .. ' file' or 'Editing a file'
 	end,
 	file_explorer_text = 'Browsing files',
 	git_commit_text = 'Commiting changes',
 	plugin_manager_text = 'Managing plugins',
 	reading_text = 'Reading a file',
 	workspace_text = 'Working on a project',
-	main_image = 'file'
+	main_image = 'neovim'
 })
